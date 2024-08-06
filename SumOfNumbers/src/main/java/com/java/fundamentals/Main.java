da415ca9f042efcfe4e743935a5a9e9bcd8ec7ad
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-06 14:56:23 +0200

    Added comments for readability

--- SumOfNumbers/src/main/java/com/java/fundamentals/Main.java
+++ SumOfNumbers/src/main/java/com/java/fundamentals/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void sumEvenNumbers(int[] arr) {
+    public static void sumEvenNumbers(int[] arr) {  //method for user input for size of the array
 
-        Scanner numbers = new Scanner(System.in);
+        Scanner numbers = new Scanner(System.in);  //user input of the numbers in array
         int size = arr.length;
 
-        System.out.println("Enter the elements of the array");
+        System.out.println("Enter the elements of the array");  //iterating over the numbers in the array
         for (int i = 0; i < size; i++) {
             arr[i] = numbers.nextInt();
         }
@@ -20,27 +20,27 @@ public class Main {
         int sum = 0;
 
         try {
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);   //user entering the size for array as an argument
 
             System.out.print("Enter the size of the array: ");
             int size = scanner.nextInt();
 
-            int[] userInputArray = new int[size];
+            int[] userInputArray = new int[size];  //passing the size parameter as an argument for the array length
             sumEvenNumbers(userInputArray);
 
-            System.out.println("The elements of the array are: ");
+            System.out.println("The elements of the array are: ");   // printing the elements of array
 
 
-            for (int j : userInputArray) {
+            for (int j : userInputArray) {   // checking if the number is even or not
                 if (j % 2 == 0) {
-                    sum = sum + j;
+                    sum = sum + j;   // if its even sum the number
                 }
             }
         } catch (Exception e) {
-            System.out.println("You can't input that");
+            System.out.println("You can't input that");  // error handling for the array elements
         }
 
 
-        System.out.println("The sum of even numbers is: " + sum);
+        System.out.println("The sum of even numbers is: " + sum);  // printing the sum of even numbers in an array
     }
 }
